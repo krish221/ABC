b5a2f13b370906751e02d9723f89f3056f983617
shreekisan <koiralashreekrishna@gmail.com>
2021-02-18 13:16:19 -0600

    shreekisan change

---
+++ src/hello/TC_add_shree.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class TC_add_shree {
+
+	public static void main(String[] args) {
+		System.out.println(" this is add from koiralashreekrishna account / from dell "
+				+ "laptop when clone the url from krishkoirala23 which is master copy in hp computerl.");
+		
+	}
+
+}
